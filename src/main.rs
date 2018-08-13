b7c706c1f3c56e3b5947ab2cbd06bd6a5031cd5b
onigra <3280467rec@gmail.com>
2018-08-13 23:49:44 +0900

    describe instance

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+extern crate rusoto_core;
+extern crate rusoto_ec2;
+
+use rusoto_core::Region;
+use rusoto_ec2::{DescribeInstancesRequest, Ec2, Ec2Client, Filter};
+
+fn main() {
+    let client = Ec2Client::new(Region::ApNortheast1);
+
+    let filter = Filter {
+        name: Some(String::from("tag:Name")),
+        values: Some(vec![String::from("web")]),
+    };
+
+    let describe_instance_request = DescribeInstancesRequest {
+        dry_run: Some(false),
+        filters: Some(vec![filter]),
+        instance_ids: None,
+        max_results: None,
+        next_token: None,
+    };
+
+    match client.describe_instances(describe_instance_request).sync() {
+        Ok(output) => match output.reservations {
+            Some(instances) => {
+                println!("{:#?}", instances);
+            }
+            None => println!("No Instances"),
+        },
+        Err(error) => {
+            println!("Error: {:#?}", error);
+        }
+    }
+}
